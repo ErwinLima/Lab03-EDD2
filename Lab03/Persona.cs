8aa820b92351bdf7ea48853f45fd1ffc15928a9e
ErwinLima <ealiman@correo.url.edu.gt>
2022-09-22 12:11:07 -0600

    AVL tree, Nodo and Persona class added

---
+++ Lab03/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    public class Persona
+    {
+        public string name { get; set; }
+        public string dpi { get; set; }
+        public DateTime datebirth { get; set; }
+        public string address { get; set; }
+        public string[] companies { get; set; }                                
+    }
+}
